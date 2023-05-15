8ea28abb0124ea02eb6ba4537027965b2dca0326
Leonardo Ruiz Bottura <leobottura12@gmail.com>
2023-05-15 15:00:42 -0300

    feat: Curinga Get/Put Contravariancia

---
+++ curingas_get_put_contravariancia/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args){
+
+        List<Object> myObjs = new ArrayList<>();
+        myObjs.add("Maria");
+        myObjs.add("Alex");
+
+        List<? super Number> myNums = myObjs;
+
+        myNums.add(10);
+        myNums.add(3.14);
+
+        //Number x = myNums.get(0);
+
+        // GET - Error
+        // PUT - OK
+    }
+}
