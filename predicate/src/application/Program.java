9071b19b28149c4d5a53e0663311b8cccb899ed2
Leonardo Ruiz Bottura <leobottura12@gmail.com>
2023-05-16 15:30:51 -0300

    refactor: PREDICATE (Lambda declarada)

--- predicate/src/application/Program.java
+++ predicate/src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -19,8 +19,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        //Referência para método
-        list.removeIf(Product::nonstaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+
+        list.removeIf(pred);
 
         for (Product p : list){
             System.out.println(p);
