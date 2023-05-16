335a8ad28b3ae1b2b5103cc32e4eaf99cb540cec
Leonardo Ruiz Bottura <leobottura12@gmail.com>
2023-05-16 15:55:27 -0300

    refactor: CONSUMER (Lambda declarada)

--- consumer/src/application/Program.java
+++ consumer/src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import entities.Product;
-import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
 
@@ -19,7 +19,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        Consumer<Product> cons = p -> {
+            p.setPrice(p.getPrice() * 1.1);
+        };
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
     }
